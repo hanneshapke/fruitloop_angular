37ebb0e6e52a39e641cfd7ee77a247f7cfa22254
Hannes Hapke <hannes.hapke@gmail.com>
2014-08-19 12:18:48 -0700

    Introduction of the leaflet map
    - installed bootstrap, leaflet and the AngularJS directive for leaflet
      via bower
    - introduced the ngResource for load the API data and display it on the
      leaflet map

--- fruitloop_angular/fruitloop_angular/assets/js/fruitloop/controller.js
+++ fruitloop_angular/fruitloop_angular/assets/js/fruitloop/controller.js
@@ -1,10 +1,47 @@
 // initialize an angular app with the name fruitApp
 // load dependencies here if needed
-var fruitApp = angular.module('fruitApp', ['fruitServices']);
+var fruitApp = angular.module('fruitApp', ['leaflet-directive', 'fruitServices']);
 
 // create an anguar controller to serve the static data
-fruitApp.controller('FruitLocationCtrl', function ($scope, FruitLocationService) {
-    $scope.region = 'World';
-    // retrieve the data from API endpoint
-    $scope.fruitLocations = FruitLocationService.query();
+fruitApp.controller('FruitLocationCtrl', function ($scope, leafletData, FruitLocationService) {
+
+    // set variables
+    $scope.markers = new Array();
+    var bbox = new Array();
+
+    // retrieve the data from API endpoint with initial load
+    FruitLocationService.query().$promise.then(
+      function (response) {
+        // copy the data to the $scope for the table to show
+        $scope.fruitLocations = response;
+        // loop through the data set and set the markers on the map
+        $scope.fruitLocations.forEach(function(fruitLocation){
+          $scope.markers.push({
+            // convert from str to float
+            lat: parseFloat(fruitLocation.latitude),
+            lng: parseFloat(fruitLocation.longitude)
+          });
+          // get the coordinates of the markers to determine the max bounds
+          bbox.push([
+            parseFloat(fruitLocation.latitude),
+            parseFloat(fruitLocation.longitude)
+          ]);
+        });
+        // get the map element
+        leafletData.getMap().then(function(map) {
+          // change the map to fit all markers
+          map.fitBounds(bbox);
+        });
+      }
+    );
+
+    // $scope.fruitLocations will be undefined because the API request is async
+    console.log($scope.fruitLocations);
+
+    // inital values for the leaflet map, they will be overwritten once
+    // $promise returns the data
+    $scope.center = {lat: 45.50, lng: -122.5, zoom: 10};
+
+    // default settings for the leaflet map
+    $scope.defaults = {scrollWheelZoom: false};
 });
--- fruitloop_angular/fruitloop_angular/assets/js/fruitloop/services.js
+++ fruitloop_angular/fruitloop_angular/assets/js/fruitloop/services.js
@@ -3,7 +3,7 @@ var fruitServices = angular.module('fruitServices', ['ngResource']);
 fruitServices.factory('FruitLocationService', ['$resource',
   function($resource){
     return $resource('api/', {}, {
-      query: {method:'GET', params:{}, isArray:true}
+      query: {method:'GET', params:{}, isArray: true}
     });
   }
 ]);
