c38255a6617952f0190affb0ac20ab64e2b7d400
Hannes Hapke <hannes.hapke@gmail.com>
2014-08-18 17:09:20 -0700

    Example for RelatedField option
    - Introduced related field option to display the Django ChoiceField as
      text

--- fruitloop_angular/fruitloop_angular/apps/fruit/serializers.py
+++ fruitloop_angular/fruitloop_angular/apps/fruit/serializers.py
@@ -3,9 +3,11 @@ from .models import FruitLocation
 
 
 class FruitSerializer(serializers.ModelSerializer):
+    fruit_name = serializers.RelatedField(source='fruit_type')
 
     class Meta:
         model = FruitLocation
         fields = ('id', 'address',
-                  'comment', 'fruit_type',
+                  'comment',
+                  'fruit_type', 'fruit_name',
                   'latitude', 'longitude',)
